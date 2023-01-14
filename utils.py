ebdad1191aa6b6548f98ee8c10aec3bded15a2e6
MichaelJackyson <josunthefunguy@gmail.com>
2023-01-14 20:01:51 +0800

    this is my code

--- utils.py
+++ utils.py
@@ -5,6 +5,17 @@ import yfinance as yf
 
 
 def get_stock(target: str, period="max") -> List[float]:
+    """it gets the stock prices
+
+    Args:
+        target (`str`): Stock call number required
+        period (`str`): The time of the info asked
+
+    >>> from utils import get_stock
+    >>> prices = get_stock("AMZN")
+    >>> len(prices)
+    6460
+    """
     stock = yf.Ticker(target)
 
     # rich.print(stock.info)
