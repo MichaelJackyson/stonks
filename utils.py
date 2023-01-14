7de85e27d0d7f7f9970f515911e1e789f83620a3
Henry Chao <henryxyz@Henrys-MacBook-Pro.local>
2023-01-14 18:51:05 +0800

    add default period

--- utils.py
+++ utils.py
@@ -4,14 +4,15 @@ import rich
 import yfinance as yf
 
 
-def get_stock(target: str) -> List[float]:
+def get_stock(target: str, period="max") -> List[float]:
     stock = yf.Ticker(target)
 
     # rich.print(stock.info)
 
-    hist = stock.history(period="max")
+    hist = stock.history(period=period)
     # rich.print(hist)
 
     prices = hist["Close"].tolist()
 
     return prices
+
