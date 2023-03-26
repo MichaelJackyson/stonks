221c43d5928cb1232c204fc2df5aa722bafbad44
MichaelJackyson <josunthefunguy@gmail.com>
2023-03-26 12:04:19 +0800

    hello. Plot graph to function

--- plot_test.py
+++ plot_test.py
@@ -1,3 +1,5 @@
+import datetime
+from typing import List
 import plotly.graph_objects as go
 
 from utils import get_stock, get_date_list
@@ -7,15 +9,19 @@ prices = get_stock("AMZN")
 
 date_list = get_date_list("AMZN")
 
-fig = go.Figure()
+def draw_graph(date_list: List[datetime.datetime], prices: List[float]) -> None:
+    fig = go.Figure()
 
-fig.add_trace(
-    go.Scatter(
-        x=date_list, y=prices,
-        mode="lines",
-        name="abc",
-        line=dict(width=2)
+    fig.add_trace(
+        go.Scatter(
+            x=date_list, y=prices,
+            mode="lines",   
+            name="abc",
+            line=dict(width=2)
+        )
     )
-)
 
-fig.show()
+    fig.show()
+
+
+draw_graph(date_list, prices)
