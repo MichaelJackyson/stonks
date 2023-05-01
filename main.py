dc1d8ad051997a82af63497b1f125653cb4d2ef8
MichaelJackyson <josunthefunguy@gmail.com>
2023-05-01 10:29:52 +0800

    currently yeeting my friends in roblox

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from utils import get_stock, get_date_list, get_stock_price_date, draw_graph
 from core import me1, me2
 
 
-# prices = get_stock("TSLA ")
+# prices = get_stock("TSLA")
 price_date = get_stock_price_date(target="ETH-USD", period="3y")
-print(price_date)
+prices = list(price_date.values())
+
+
+def average(prices):
+    return sum(prices) / len(prices)
+
+
+print(average(prices))
+exit()
+
 
 draw_graph(list(price_date.keys()), list(price_date.values()))
 
